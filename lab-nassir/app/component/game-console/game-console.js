de29beedf3fea4d9670cc4d33f99db20962c97b2
Nassir Isaf <njisaf@gmail.com>
2016-10-20 15:07:32 -0700

    fixed broken components

--- lab-nassir/app/component/game-console/game-console.js
+++ lab-nassir/app/component/game-console/game-console.js
@@ -3,7 +3,7 @@
 const angular = require('angular');
 const ngAdventure = angular.module('ngAdventure');
 
-ngAdventure.component('gameControl', {
+ngAdventure.component('gameConsole', {
   template: require('./game-console.html'),
   controller: 'GameConsoleController',
   controllerAs: 'gameConsoleCtrl',
